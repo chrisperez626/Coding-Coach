7a45faf7e3bb9a4e2d02b05da07a63c96a40ed0a
Alex Scarlett <alex.scarlett91@gmail.com>
2018-04-27 11:40:06 -0400

    Added HTML route for contribution post, renamed handlebars files to match routes

--- controller/html-routes.js
+++ controller/html-routes.js
@@ -6,9 +6,14 @@ module.exports = function(app) {
     res.render('index', dataObject);
   });
 
-  app.get('/contributions', function(req, res) {
+  app.get('/resources', function(req, res) {
     var dataObject = {};
     res.render('resources', dataObject);
+  });
+
+  app.get('/contributions', function(req, res) {
+    var dataObject = {};
+    res.render('contributions', dataObject);
   });
 
   app.get('/contributions/post', function(req, res) {
