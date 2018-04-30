96ef5fbbfdaaa321658fa9d5d244954456e2b897
Alex Scarlett <alex.scarlett91@gmail.com>
2018-04-30 12:02:12 -0400

    Added function to make sure all fields are filled out before post submit

--- public/js/post.js
+++ public/js/post.js
@@ -1,15 +1,43 @@
-$(document).ready(function () {
-    $('#post').on('submit', function () {
-        var postObject = {
-            name: $('#user').val().trim(),
-            title: $('#title').val().trim(),
-            content: $('#body').val().trim(),
-            language: $('#languages').val(),
-            content_type: $('#post-type').val()
-        };
+$(document).ready(function() {
+  $('#post').on('submit', function(event) {
+    event.preventDefault();
 
-        $.post('/api/contributions', postObject, function (data) {
-            console.log(data);
-        });
-    });
+    function postCompletion() {
+      var completedPost = true;
+
+      if (
+        $('#user').val() === '' ||
+        $('#title').val() === '' ||
+        $('#body').val() === '' ||
+        $('#languages').val() === '' ||
+        $('#post-type').val() === ''
+      ) {
+        completedPost = false;
+      }
+
+      return completedPost;
+    }
+
+    if (postCompletion()) {
+      var postObject = {
+        name: $('#user')
+          .val()
+          .trim(),
+        title: $('#title')
+          .val()
+          .trim(),
+        content: $('#body')
+          .val()
+          .trim(),
+        language: $('#languages').val(),
+        content_type: $('#post-type').val()
+      };
+
+      $.post('/api/contributions', postObject, function(data) {
+        console.log(data);
+      });
+    } else {
+      alert('Please fill out all fields to submit a post!');
+    }
+  });
 });
