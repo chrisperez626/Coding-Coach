3ec3fd1230c19d7292d3d547fe5fbfe608b73569
chrisperez626 <chrisperez626@gmail.com>
2018-05-01 18:01:31 -0400

    fixing snippets

--- public/js/contributions.js
+++ public/js/contributions.js
@@ -1,43 +1,61 @@
-$(document).ready(function() {
-  (function() {
-    var showChar = 300;
-    var ellipsestext = '...';
+$(document).ready(function () {
 
-    $('.truncate').each(function() {
-      var content = $(this).html();
-      if (content.length > showChar) {
-        var c = content.substr(0, showChar);
-        var h = content;
-        var html =
-          '<div class="truncate-text" style="display:block">' +
-          c +
-          '<span class="moreellipses">' +
-          ellipsestext +
-          '&nbsp;&nbsp;<strong><a href="" class="moreless more">read more</a></strong></span></span></div><div class="truncate-text" style="display:none">' +
-          h +
-          '&nbsp;&nbsp;<strong><a href="" class="moreless less"> less</a></strong></span></div>';
+  var showChar = 300;
+  var ellipsestext = '...';
 
-        $(this).html(html);
-      }
-    });
+  $('.truncate').each(function (){
+    var content = $(this).html();
+    console.log("LENGTH: ", content.length);
+    
+    if (content.length > showChar) {
+      var c = content.substr(0, showChar);
+      var index = c.lastIndexOf(">", 0);
 
-    $('.moreless').click(function() {
-      var thisEl = $(this);
-      var cT = thisEl.closest('.truncate-text');
-      var tX = '.truncate-text';
+      c = c.substr(0, index >= 0 ? index + 1 : 300);
+      // console.log(c);
 
-      if (thisEl.hasClass('less')) {
-        cT.prev(tX).toggle();
-        cT.slideToggle();
-      } else {
-        cT.toggle();
-        cT.next(tX).fadeToggle();
-      }
-      return false;
-    });
-  })();
+      var h = content;
+      var html = `
+          <div class="truncate-text" style="display:block">
+            ${c}
+            <span class="moreellipses">
+              ${ellipsestext}
+              &nbsp;&nbsp;<strong><a href="" class="moreless more">read more</a></strong>
+            </span>
+          </div>
+          <div class="truncate-text" style="display:none">
+            ${h}
+            <span>&nbsp;&nbsp;<strong><a href="" class="moreless less">less</a></strong></span>
+          </div>
+        `;
+      // '<div class="truncate-text" style="display:block">' +
+      // c +
+      // '<span class="moreellipses">' +
+      // ellipsestext +
+      // '&nbsp;&nbsp;<strong><a href="" class="moreless more">read more</a></strong></span></span></div><div class="truncate-text" style="display:none">' +
+      // h +
+      // '&nbsp;&nbsp;<strong><a href="" class="moreless less"> less</a></strong></span></div>';
 
-  $('select[name="language"]').change(function() {
+      $(this).html(html);
+    }
+  });
+
+  $('body').on('click', '.moreless', function () {
+    var thisEl = $(this);
+    var cT = thisEl.closest('.truncate-text');
+    var tX = '.truncate-text';
+
+    if (thisEl.hasClass('less')) {
+      cT.prev(tX).toggle();
+      cT.slideToggle();
+    } else {
+      cT.toggle();
+      cT.next(tX).fadeToggle();
+    }
+    return false;
+  });
+
+  $('select[name="language"]').change(function () {
     var language = $('select[name=language]').val();
     console.log(language);
     location.href = '/api/contributions/' + language;
